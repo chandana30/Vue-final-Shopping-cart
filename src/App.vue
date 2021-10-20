c0602643a6d1add9b6db61f22d45885097d9f6db
sachinarbhat <sachinarbhat@gmail.com>
2021-10-20 11:24:04 +0530

    Shopping cart

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+  <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components:{
+
+  },
+  data() {
+    return {
+      checkedData: [],
+    };
+  },
+  created() {
+    let user= localStorage.getItem('user')
+    if(!user){
+      this.$router.push({name:"login"})
+    }
+  },
+};
 </script>
 
 <style>
@@ -19,7 +29,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
