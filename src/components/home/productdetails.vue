06d4ccc88573f392a04d3ee8babf723633f4bd94
sachinarbhat <sachinarbhat@gmail.com>
2021-10-21 10:49:34 +0530

    Some changes

--- src/components/home/productdetails.vue
+++ src/components/home/productdetails.vue
@@ -65,7 +65,7 @@ export default {
       const cartthings = JSON.parse(localStorage.getItem("cart"));
       cartthings.push({ "id": thing, "title": thingname, "image": thingimage, "price": thingprice, "total": thingprice, "quantity": thingquant });
       localStorage.setItem("cart", JSON.stringify(cartthings));
-      this.itemdetails = JSON.parse(localStorage.getItem("cart"));
+    
         },
     },
 }
